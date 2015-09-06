4afb603722f4f3cbe1b009747b2c233721c2b711
Jaga <salamander.g.1@gmail.com>
2015-09-06 21:55:55 +0900

    検索機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,10 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.all
+  #  @books = Book.all
+  @q        = Book.search(params[:q])
+    @books = @q.result(distinct: true)
+
   end
 
   # GET /books/1
